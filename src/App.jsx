87ef77f306c78f793b03e39fab5a289de0a3808a
Carlos Hernández <carlos_develops@outlook.com>
2022-08-08 16:33:49 -0500

    init project + tailwind

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header"
+import Form from "./components/Form"
+import PatientList from "./components/PatientList"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Form />
+      <PatientList />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/PatientList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PatientList = () => {
+  return (
+    <h2>Listado de pacientes</h2>
+  )
+}
+
+export default PatientList
