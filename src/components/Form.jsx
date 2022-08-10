d96cf10149ebf0882022a3320ffb40c6e57d9dd7
Carlos Hernández <carlos_develops@outlook.com>
2022-08-10 09:02:33 -0500

    formulario y listado de pacientes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -9,8 +9,8 @@ const Form = () => {
         <span className='text-indigo-600 font-bold'>y Administra Pacientes</span>
       </p>
 
-      <form className='bg-white shadow-sm rounded-sm py-10 px-5' action="">
-        <div>
+      <form className='bg-white shadow-sm rounded-sm py-10 px-5 mb-10' action="">
+        <div className='mb-5'>
           <label
             htmlFor='mascota'
             className='block text-gray-700 uppercase font-bold'
@@ -22,6 +22,60 @@ const Form = () => {
             placeholder='Nombre de la mascota'
           />
         </div>
+        <div className='mb-5'>
+          <label
+            htmlFor='mascota'
+            className='block text-gray-700 uppercase font-bold'
+            >Propietario</label>
+          <input
+            id="propietario"
+            className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-sm'
+            type="text"
+            placeholder='Nombre del propietario'
+          />
+        </div>
+        <div className='mb-5'>
+          <label
+            htmlFor='email'
+            className='block text-gray-700 uppercase font-bold'
+            >Email</label>
+          <input
+            id="email"
+            className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-sm'
+            type="email"
+            placeholder='Email'
+          />
+        </div>
+        <div className='mb-5'>
+          <label
+            htmlFor='email'
+            className='block text-gray-700 uppercase font-bold'
+            >Fecha de Alta</label>
+          <input
+            id="alta"
+            className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-sm'
+            type="date"
+            placeholder='Fecha de alta del paciente'
+          />
+        </div>
+        <div className='mb-5'>
+          <label
+            htmlFor='sintomas'
+            className='block text-gray-700 uppercase font-bold'
+            >Síntomas</label>
+          <textarea
+            name=""
+            id="sintomas"
+            className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-sm'
+            placeholder='Descricción de los síntomas'>
+            </textarea>
+        </div>
+
+        <input
+          type="sumbit"
+          className='bg-indigo-600 w-full p-3 text-white uppercase font-bold text-center hover:bg-indigo-700 transition-colors cursor-pointer'
+          value="Agregar paciente"
+        />
       </form>
     </div>
   )
---
+++ src/components/Patient.jsx
@@ -0,0 +1,23 @@
+const Patient = () => {
+  return (
+    <div className='m-3 bg-white shadow-sm px-5 py-10 rounded-xl'>
+        <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
+          <span className='font-normal normal-case'>Blondi</span>
+        </p>
+        <p className='font-bold mb-3 text-gray-700 uppercase'> Propietario: {''}
+          <span className='font-normal normal-case'>Carlos</span>
+        </p>
+        <p className='font-bold mb-3 text-gray-700 uppercase'> Email: {''}
+          <span className='font-normal normal-case'>carlos_develops@outlook.com</span>
+        </p>
+        <p className='font-bold mb-3 text-gray-700 uppercase'> Fecha Alta: {''}
+          <span className='font-normal normal-case'>10 Diciembre 2022</span>
+        </p>
+        <p className='font-bold mb-3 text-gray-700 uppercase'> Síntomas: {''}
+          <span className='font-normal normal-case'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, sit? Esse cupiditate aliquam repellendus architecto voluptates, porro rem eligendi, corrupti doloribus eveniet facere modi nesciunt autem ad sit odio perferendis!</span>
+        </p>
+    </div>
+  )
+}
+
+export default Patient
