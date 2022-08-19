9427f2ac30cdb4887bf639ca35db845a5e3e1683
Carlos Hernández <carlos_develops@outlook.com>
2022-08-19 11:47:16 -0500

    validando formularios

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
+import {useState, useEffect} from 'react';
 
 const Form = () => {
+
+  const [name, setName] = useState('')
+  const [owned, setOwned] = useState('')
+  const [email, setEmail] = useState('')
+  const [date, setDate] = useState('')
+  const [symptom, setSymptom] = useState('')
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    // Validate form:
+    if( [name, owned, email, date, symptom].includes('') ) {
+      console.log('Hay al menos un campo vacío')
+    }
+
+
+  }
+
   return (
-    <div className='md:w-1/2 lg:2/5'>
+    <div className='md:w-1/2 lg:2/5 mx-5'>
       <h2 className='font-black text-3xl text-center'>Seguimiento pacientes</h2>
       <p className='text-lg my-5 text-center'>
         Añade {''}
         <span className='text-indigo-600 font-bold'>y Administra Pacientes</span>
       </p>
 
-      <form className='bg-white shadow-sm rounded-sm py-10 px-5 mb-10' action="">
+      <form
+        className='bg-white shadow-sm rounded-sm py-10 px-5 mb-10'
+        onSubmit={ handleSubmit }
+      >
+        {/* petName */}
         <div className='mb-5'>
           <label
             htmlFor='mascota'
@@ -20,11 +43,14 @@ const Form = () => {
             className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-sm'
             type="text"
             placeholder='Nombre de la mascota'
+            value={name}
+            onChange={ (e) => setName(e.target.value) }
           />
         </div>
+        {/* pet owned name */}
         <div className='mb-5'>
           <label
-            htmlFor='mascota'
+            htmlFor='owned'
             className='block text-gray-700 uppercase font-bold'
             >Propietario</label>
           <input
@@ -32,8 +58,11 @@ const Form = () => {
             className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-sm'
             type="text"
             placeholder='Nombre del propietario'
+            value={owned}
+            onChange={ (e) => setOwned(e.target.value) }
           />
         </div>
+        {/* Email */}
         <div className='mb-5'>
           <label
             htmlFor='email'
@@ -44,38 +73,44 @@ const Form = () => {
             className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-sm'
             type="email"
             placeholder='Email'
+            value={email}
+            onChange={ (e) => setEmail(e.target.value) }
           />
         </div>
+        {/* Fecha de alta */}
         <div className='mb-5'>
           <label
-            htmlFor='email'
+            htmlFor='date'
             className='block text-gray-700 uppercase font-bold'
             >Fecha de Alta</label>
           <input
-            id="alta"
+            id="date"
             className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-sm'
             type="date"
             placeholder='Fecha de alta del paciente'
+            value={date}
+            onChange={ (e) => setDate(e.target.value) }
           />
         </div>
+        {/* symptom */}
         <div className='mb-5'>
           <label
-            htmlFor='sintomas'
+            htmlFor='symptom'
             className='block text-gray-700 uppercase font-bold'
             >Síntomas</label>
           <textarea
             name=""
-            id="sintomas"
+            id="symptom"
             className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-sm'
-            placeholder='Descricción de los síntomas'>
+            placeholder='Descricción de los síntomas'
+            value={symptom}
+            onChange={ (e) => setSymptom(e.target.value) }
+            >
             </textarea>
         </div>
-
-        <input
-          type="sumbit"
+        <button
           className='bg-indigo-600 w-full p-3 text-white uppercase font-bold text-center hover:bg-indigo-700 transition-colors cursor-pointer'
-          value="Agregar paciente"
-        />
+          type="submit">Agregar paciente</button>
       </form>
     </div>
   )
--- src/components/Patient.jsx
+++ src/components/Patient.jsx
@@ -1,6 +1,6 @@
 const Patient = () => {
   return (
-    <div className='m-3 bg-white shadow-sm px-5 py-10 rounded-xl'>
+    <div className='mx-3 my-5 bg-white shadow-sm px-5 py-10 rounded-xl'>
         <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre: {''}
           <span className='font-normal normal-case'>Blondi</span>
         </p>
